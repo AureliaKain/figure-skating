da654a938d0106164ae8e5e145265844d41ac6aa
Aurelia Kain <aureliakain@MacBook-Pro-de-Aurelia.local>
2019-11-12 20:15:01 +0100

    quick fix

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,15 +1,14 @@
 import sqlite3
 
-from flask import Blueprint, session, g
+from flask import Blueprint, session
 from flask import render_template
-from flask import request
 from flask import url_for
 import os
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
-bp = Blueprint("main", __name__, template_folder='templates')
+bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
@@ -21,15 +20,10 @@ def index():
         return "Hello Boss! <a href=/logout>Logout</a>"
 
 
-@bp.route("/figures_list_auto/")
+@bp.route("/figures_list/")
 def figures_list_auto():
     figures = get_db().cursor().execute("SELECT * FROM figure").fetchall()
-    return render_template("main/figures_list_auto.html", figures=figures)
-
-
-@bp.route("/figures_list")
-def figures_list():
-    return render_template('main/figures_list.html')
+    return render_template("main/figures_list.html", figures=figures)
 
 
 @bp.route("/profile")
